f8cca32fd8b3357c14c9a3d70336280c6e964265
Vladislav Shmychkov <124574156+shmyCHA-CHA@users.noreply.github.com>
2023-03-02 10:39:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 2:
+// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+// Console.WriteLine("Введите первое число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+
+
+
+
+// Задача 4:
+// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+// System.Console.WriteLine("Максимальное из этих чисел = " + max);
+
+
+
+// Задача 6: 
+// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+// System.Console.WriteLine("Число:" + a + "четное");
+// else System.Console.WriteLine($"Число: {a} нечетное");
+
+
+ 
+// Задача 8:
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+// Console.WriteLine("Введите число N");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for(int i=1;i<=n;i++)
+// {
+// if (i % 2 == 0)
+// System.Console.Write(i + "," , ",");
+// }
+
